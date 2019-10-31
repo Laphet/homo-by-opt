155a8989b1bee0918d31103e3b771ae220e18fe5
cy2571 <cy2571@columbia.edu>
2019-10-31 18:33:48 -0400

    Zzzzzzzzzzzzzzzzzzzzzz

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 bsub -J homo -q batch -R "span[ptile=36]" -n 8 -e .err -o tmp.out "python test_ex1.py 32 32"
+sleep 10
 bsub -J homo -q batch -R "span[ptile=36]" -n 8 -e .err -o tmp.out "python test_ex1.py 16 32"
