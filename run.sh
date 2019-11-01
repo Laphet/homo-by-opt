3a444b9e7cf4b46c99a7910c8feff9785e574a8c
cy2571 <cy2571@columbia.edu>
2019-11-01 15:34:11 -0400

    sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ sleep 10
 bsub -J homo -q batch -R "span[ptile=36]" -n 8 -e output/b8r32.err -o output/b8r32.out "python test_ex1.py 8 32"
 sleep 10
 bsub -J homo -q batch -R "span[ptile=36]" -n 8 -e output/b16r32.err -o output/b16r32.out "python test_ex1.py 16 32"
-sleep
+sleep 10
 bsub -J homo -q batch -R "span[ptile=36]" -n 8 -e output/b32r32.err -o output/b32r32.out "python test_ex1.py 32 32"
