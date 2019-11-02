a2a42b8339d73e3dc13adb3adf5d2c3fbc7e5d06
cy2571 <cy2571@columbia.edu>
2019-11-02 17:04:49 -0400

    Add preconditioner

--- test_ex1.py
+++ test_ex1.py
@@ -1,23 +1,24 @@
 from StructuredMesh2D import Mesh, Variable, get_mass_matrix_opt, get_stiffness_matrix_opt
 import numpy as np
-from scipy.sparse.linalg import cg, gmres
+from scipy.sparse.linalg import cg, gmres, spsolve, LinearOperator
+from scipy.sparse import csr_matrix
 import time
 import datetime
 import logging
 import sys
 
 
-LOG_FORMAT = "%(asctime)s %(levelname)s \t%(message)s"
-DATE_FORMAT = "%m/%d/%Y %H:%M:%S"
+def _get_preconditioner(op: csr_matrix):
+    assert (op.shape[0] == op.shape[1])
+    N = op.shape[0]
+    diagonal_mat = csr_matrix(
+        (op.diagonal(), (np.arange(N), np.arange(N))), shape=(N, N))
 
-NOW = datetime.datetime.now()
-logging.basicConfig(filename='ycq-'+NOW.strftime("%m-%d-%Y-%H-%M-%S")+'.log',
-                    level=logging.INFO,
-                    format=LOG_FORMAT,
-                    datefmt=DATE_FORMAT)
+    def M_x(x): return spsolve(diagonal_mat, x)
+    M = LinearOperator((N, N), M_x)
+    return M
 
 
-logging.info("Go Go Go!")
 base_grid = 2
 refine_num = 2
 LSolver = cg
@@ -30,11 +31,20 @@ try:
         refine_num = int(sys.argv[2])
         assert refine_num >= 2
 except:
-    logging.error("Invaild arguments, use default values instead.")
+    print("Invaild arguments, use default values instead.")
     base_grid = 16
     refine_num = 32
 
-logging.info("Use parameters base_grid=%d, refine_num=%d.",
+cfg = "b{0:d}r{1:d}".format(base_grid, refine_num)
+LOG_FORMAT = "%(asctime)s %(levelname)s \t%(message)s"
+DATE_FORMAT = "%m/%d/%Y %H:%M:%S"
+
+NOW = datetime.datetime.now()
+logging.basicConfig(filename="log/"+cfg+NOW.strftime("-%m-%d-%Y-%H-%M-%S")+'.log',
+                    level=logging.INFO,
+                    format=LOG_FORMAT,
+                    datefmt=DATE_FORMAT)
+logging.info("Go Go Go! with config base_grid=%d, refine_num=%d.",
              base_grid, refine_num)
 
 coarse_mesh = Mesh(base_grid)
@@ -48,6 +58,8 @@ co_on_coarse_mesh.data = np.ones(co_on_coarse_mesh.data.shape)
 start = time.time()
 stiff_mat_ff = get_stiffness_matrix_opt(co_on_fine_mesh)
 stiff_mat_cc = get_stiffness_matrix_opt(co_on_coarse_mesh)
+pre_ff = _get_preconditioner(stiff_mat_ff)
+pre_cc = _get_preconditioner(stiff_mat_cc)
 
 mass_mat_f2f2 = get_mass_matrix_opt(fine_mesh,
                                     Variable.TYPE_DIC["first-order-zeroBC"],
@@ -78,7 +90,7 @@ for i in range(coarse_mesh.inner_node_count):
     mass_mat_c2f2[i, :] = mass_mat_f2f2.dot(_base_c2_refined.data)
 A = np.zeros(mass_mat_c2f2.shape)
 for j in range(fine_mesh.inner_node_count):
-    A[:, j], info = LSolver(mass_mat_c2c2, mass_mat_c2f2[:, j])
+    A[:, j], info = LSolver(mass_mat_c2c2, mass_mat_c2f2[:, j], M=pre_cc)
 end = time.time()
 logging.info("Finishing constructing Mat A, consuming time=%.3fs.", end-start)
 
@@ -90,7 +102,7 @@ for j in range(coarse_mesh.elem_count):
     _base_c0.data[j] = 1.0
     _base_c0_refined = _base_c0.project_to_refined_mesh(refine_num)
     B[:, j] = mass_mat_f2f0.dot(_base_c0_refined.data)
-    B[:, j], info = LSolver(stiff_mat_ff, B[:, j])
+    B[:, j], info = LSolver(stiff_mat_ff, B[:, j], M=pre_ff)
     if (j % coarse_mesh.M == coarse_mesh.M-1):
         batch_end = time.time()
         logging.info("\tBatch %d/%d completed, consuming time=%.3fs;",
@@ -99,8 +111,10 @@ for j in range(coarse_mesh.elem_count):
 
 end = time.time()
 logging.info("Finishing constructing Mat B, consuming time=%.3fs.", end-start)
-
-diff = stiff_mat_cc.toarray().dot(A.dot(B)) - mass_mat_c2c0.toarray()
+mat_AB = np.matmul(A, B)
+diff = np.matmul(stiff_mat_cc.toarray(), mat_AB) - mass_mat_c2c0.toarray()
+with open("data/"+cfg+"-mat-AB.npy", "w") as f:
+    np.save(f.name, arr=mat_AB)
 logging.info("The absolute difference in 2-norm=%.5f.",
              np.linalg.norm(diff, ord=2))
 logging.info("The relative difference in 2-norm=%.5f.",
